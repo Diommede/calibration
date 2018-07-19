022b757d260087744cddad0ed66bb3a1aef13919
De Oliveira <denisetienne.oliveira@gmail.com>
2018-07-19 16:54:22 +0200

    Iteration sur toutes les mesures et synchronisation

--- Autocalib.py
+++ Autocalib.py
@@ -39,66 +39,101 @@ class main:
 					z = z.split(',')
     			z1 = [float(i) for i in z]
 			Nw_lect = np.array(z1)		
-					
-#Lecture de la longueur mesurée et de la position par rapport au monde
+
+#Lecture de la longueur mesurée par rapport au monde
 		tab_lidars = []
    		bag = rosbag.Bag('/home/denis/BAGS/mesure2.bag', 'r')
 		for (topic, msg, t) in bag.read_messages(topics = 'mappys'):
-			tab_lidars.append([msg,t])
-			
-			print tab_lidars
-		
+			tab_lidars.append([msg.data, t.to_sec()])
+		tab_lidars = np.array(tab_lidars)
 
-		l_lect = msg.data[3]
+#Lecture de la position camera
+#tag detection periode = 50ms
 		proxi = []
-##tag detection periode = 50ms
-#		for (topic, msg, t) in bag.read_messages(topics = 'tf'):
-#			position_wb_lect = msg.transforms[0].transform
+		for (topic, msg, t) in bag.read_messages(topics = 'tf'):
+			proxi.append([msg.transforms[0].transform, msg.transforms[0].header.stamp.to_sec()])
+		proxi = np.array(proxi)
+#Moyennage des valeurs de lidars autour des temps de position
+		Liste_moyennage = []								# Liste des valeurs à moyenner
+												# 	Nombre de mesures lidars "synchrones"
+												# 		  ............	
+												#                |           |
+												#                                  _    _
+												# 	    _	 ____________   / | |    :  	 
+												#  Nombre  :	 |_|_|_|_|_|_| /  | |    :  Nombre de
+												#    de	   :	 |_|_|_|_|_|_|    | |    :   lidars
+												#  mesures :	 |_|_|_|_|_|_| \  | |    :     6
+												#  camera  :_	 |_|_|_|_|_|_|  \ |_|   _:  
+												#    343                                        												#						
 
-#	#Moyennage des valeurs de lidars autour des temps de position
-#			while i < len(tab_lidars)
-#				if  = tab_lidars[i][2]
-#					proxi[i]
-#			print e
+		moyennage = []									# Liste des valeurs moyennées
+												# 		 Nombre de lidars	
+												# 		  ............	
+												#                |           |
+												#                                 
+												# 	    _	 ____________   	 
+												#  Nombre  :	 |_|_|_|_|_|_| 
+												#    de	   :	 |_|_|_|_|_|_|
+												#  Mesures :	 |_|_|_|_|_|_|
+												#  Camera  :_	 |_|_|_|_|_|_|
+												
+                                           
+		for j in range(len(proxi)):   							# Pour chaque mesure de camera... 
+			Liste_moyennage.append([])		
+			moyennage.append([])										
+			for i in range(len(tab_lidars)):					# Pour chaque mesure de lidar...
+				if (abs(tab_lidars[i][1] - proxi[j][1]) < 0.025):		# Si le temps séparant les deux mesures est inferieur à 
+												# 0.25s (demi période de la caméra) 
+					Liste_moyennage[j].append(tab_lidars[i][0])		# On liste toutes les mesures de lidar remplissant ce critère
+
+			for k in range(len(tab_lidars[1][0])):
+				var = []
+				for l in range (len(Liste_moyennage[j])):
+					var.append(Liste_moyennage[j][l][k])			
+				moyennage[j].append(np.mean(var))				# On fait leur moyenne de la liste ci-dessus sur les mesures 
+												# de caméra
+		moyennage = np.array(moyennage)											
 
 		bag.close()
-		rospy.spin()
 
 ####################################
 ##############CALCULS###############
 ####################################	
 #///////////////////////////////revoir l'initialisation de m
-		m = np.array([np.array([0.,0.,0.,0.,0.,0.,0.])*8])
-		b = np.array([4.]*8)
-		x = 0 
+		m = []
+		b = []
+		x = [] 
 
 		I = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]]) 
+ 
+		for i in range(len(moyennage)):
+			l_lect = moyennage[i][5]
 
-		#Remplissage structure de données
-		print l_lect
-		Donnees = Mesure (position_wb_lect, Nw_lect, l_lect)
+			position_wb_lect = proxi[0][0]
 
-		tbc = Donnees.tbc
-		rbc = Donnees.Rbcrot
+			#Remplissage structure de données
+			Donnees = Mesure (position_wb_lect, Nw_lect, l_lect)
+
+			tbc = Donnees.tbc
+			rbc = Donnees.Rbcrot
+
+			twb = Donnees.twb
+			rwb = Donnees.Rwbrot
+
+			Nw = Donnees.Nw
+			l = Donnees.l
 		
-		twb = Donnees.twb
-		rwb = Donnees.Rwbrot
+			m.append(np.concatenate((np.matmul(np.transpose(Nw), rwb) * l, np.matmul(Nw, (rwb-I)),[-1])))
+			np.append(b, np.dot(Nw,twb))
 
-		Nw = Donnees.Nw
-		l = Donnees.l
-				
-		#m = np.concatenate((np.matmul(np.transpose(Nw), rwb) * l, np.matmul(Nw, (rwb-I)),[-1]))
-		m = np.array([np.concatenate((np.matmul(np.transpose(Nw), rwb) * l, np.matmul(Nw, (rwb-I)),[-1]))]*8)
-		np.append(b, np.dot(Nw,twb))
-	
-#		print m
-#		print b
-
+		m = np.array(m)	
+		b = np.array(b)	
 
 		x = np.linalg.lstsq(m, -b)
-#		print x
 
+		print x
 
+#		rospy.spin()
 ####################################
 ###########PUBLIEUR ROS#############
 ####################################
